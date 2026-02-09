5c6ee8ed5270edc159364e53e719db94d239b8d8
Lamine Diaby <diaby.lamine@gmail.com>
2026-02-09 23:23:14 +0100

    feat(home): upcoming elections section on homepage (#40)
    Widget avec countdown animé pour l'élection la plus proche et cartes
    compactes pour les suivantes. Caché si aucune élection à venir.

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -1,3 +1,4 @@
 export { DashboardStats } from "./DashboardStats";
 export { ActivityTabs } from "./ActivityTabs";
 export { QuickTools } from "./QuickTools";
+export { UpcomingElections } from "./UpcomingElections";

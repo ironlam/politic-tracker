db15601154a3532d8fb21eacd57aa6f315590c41
Lamine Diaby <diaby.lamine@gmail.com>
2026-03-02 05:10:49 +0100

    refactor(compare): update navigation and barrel exports for redesign

--- src/components/compare/index.ts
+++ src/components/compare/index.ts
@@ -1,8 +1,4 @@
+export { CompareCategoryPicker } from "./CompareCategoryPicker";
+export { CompareSelector } from "./CompareSelector";
 export { CompareHeader } from "./CompareHeader";
-export { PoliticianSelector } from "./PoliticianSelector";
-export { PartySelector } from "./PartySelector";
-export { CompareMode } from "./CompareMode";
-export { ComparisonTable } from "./ComparisonTable";
-export { VoteAgreement } from "./VoteAgreement";
-export { PartyComparisonTable } from "./PartyComparisonTable";
 export { SuggestedComparisons } from "./SuggestedComparisons";
